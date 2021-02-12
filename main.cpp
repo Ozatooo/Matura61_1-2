052277c473d076d24f060d2a2d570a4cb5093183
Ozatooo <61974211+Ozatooo@users.noreply.github.com>
2021-02-12 11:25:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,45 +2,29 @@
 #include <fstream>
 #include <cstdlib>
 #include <math.h>
-#include <string> 
-#include <sstream> 
 #include <vector>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 using namespace std;
-
-			int ile=0;
-			int poprzednia=0;
-			string ciag;
-
-
+		int ile=0;
+		int poprzednia=0;
+		string ciag;
         bool dwucykliczny(string ciag){
         if(ciag.length()%2==0)
         {
-
         for(int i=0; i<=(ciag.length()/2)-1;i++)
         {
             if(ciag[i]!=ciag[ciag.length()/2+i])
             {
                 return false;
             }
-
-
         }
-        
-        
         for(int i=0; i<=ciag.length()-1;i++)
-        {
-        	
+        {   	
         cout<<ciag[i];
-        	
         }
         		cout<<endl;
-
-
 		    }
 		}
-		
-		
 		
 		bool spr(string ciag){
 			
@@ -51,65 +35,28 @@ using namespace std;
 					return false;
 				}
 			}
-        ile++;
+       		 ile++;
 			return true;
 		}
 		
-		
-		
-
 int main(int argc, char** argv) {
-
     fstream plik;
-
-
-
-
             plik.open("ciagi.txt", ios::in); 
             if(plik.good()) 
             while(!plik.eof()) 
                   {
-                      plik>>ciag;
-                     
-
-                   	 dwucykliczny(ciag);
-                    
-
-
+                  plik>>ciag;
+               	 dwucykliczny(ciag);
                 }
-
-             
-
-
-                 plik.close(); 
-                 
-                 
-                 
-                 
-                 
-                 
-            plik.open("ciagi.txt", ios::in); 
-            if(plik.good()) 
-            while(!plik.eof()) 
-                  {
-
-                    
+          plik.close();                 
+		        plik.open("ciagi.txt", ios::in); 
+		        if(plik.good()) 
+		        while(!plik.eof()) 
+                  {          
 					plik>>ciag;                    
                     spr(ciag);
-
                 }
-
                     cout<<ile;
-
-
                  plik.close(); 
-
-
-
-
-
-
-
-
     return 0;
 }
